93fbc0fc81f792900b82dec29bc232004f4ea91f
Fabrice <fabricevalmont1995@gmail.com>
2024-03-27 22:36:48 +0100

    Create field

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
 
-const Grid = () => {
+const GridComponent = () => {
+  // Créer un tableau vide
+  const gridArray = [];
+
+  // Boucle pour remplir le tableau avec des tableaux pour chaque ligne
+  for (let i = 0; i < 13; i++) {
+    const rowArray = [];
+    // Boucle pour remplir chaque ligne avec des cellules
+    for (let j = 0; j < 17; j++) {
+      rowArray.push(j);
+    }
+    gridArray.push(rowArray);
+  }
+
   return (
-    <div></div>
-  )
-}
+    <div className="flex justify-center items-center h-screen">
+      <div className="grid grid-cols-17 w-[680px] m-auto">
+        {gridArray.map((row, rowIndex) => (
+          row.map((_, colIndex) => {
+            // Définir la classe de fond en fonction de la position
+            let bgClass = '';
+            if (rowIndex >= 4 && rowIndex <= 8) {
+              bgClass = colIndex === 0 || colIndex === row.length - 1 ? 'bg-white' : 'bg-green-500'
+            } else if (rowIndex == 0 || rowIndex == 12) {
+              bgClass = 'bg-black'
+            }else {
+              bgClass = colIndex === 0 || colIndex === row.length - 1 ? 'bg-black' : 'bg-green-500';
+            }
+            return (
+              <div
+                key={`${rowIndex}-${colIndex}`}
+                className={`border border-black w-10 h-10 flex items-center justify-center ${bgClass}`}
+              >
+                {/* Contenu de la cellule */}
+              </div>
+            );
+          })
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Grid
+export default GridComponent;
